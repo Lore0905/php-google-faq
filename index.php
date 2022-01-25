96e7844abce1a0fa8f5f625dd7f1d84d01471976
Lore0905 <lore.castelli4@gmail.com>
2022-01-25 15:31:04 +0100

    header

--- index.php
+++ index.php
@@ -2,3 +2,61 @@
 https://policies.google.com/faq
 Ci sono diverse domande con relative risposte.
 Creare il “Database” (struttura dati) e la visualizzazione di queste domande e risposte con PHP. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Roboto&display=swap" rel="stylesheet">
+    <title>php-google-faq</title>
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <!-- first raw header -->
+        <div id="first-row-header" class="d-flex">
+            <div>
+                <img src="img/google_logo.png" alt="">
+            </div>
+            <!-- text -->
+            <h1>Privacy e termini</h1>
+        </div>
+        <!-- second raw header -->
+        <div id="second-row-header">
+            <ul class="d-flex">
+                <li>
+                    <a href="#">Introduzione</a>
+                </li>
+                <li>
+                    <a href="#">Norme sulla privacy</a>
+                </li>
+                <li>
+                    <a href="#">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="#">Tecnologie</a>
+                </li>
+                <li id="active">
+                    <a href="#">Domande frequenti</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- main -->
+    <main>
+        main
+    </main>
+
+    <!-- footer -->
+    <footer>
+        footer
+    </footer>
+    
+</body>
+</html>
